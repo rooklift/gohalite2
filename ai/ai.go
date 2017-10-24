2a1a6eeafa9b72a4687f8c6cd246f625c205ec08
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-10-24 17:44:42 +0100

    planet AI, fix slice excisions (i--)

--- ai/ai.go
+++ ai/ai.go
@@ -4,18 +4,36 @@ import (
 	hal "../gohalite2"
 )
 
+// --------------------------------------------
+
 type ShipAI struct {
+	mainAI			*AI
 	game			*hal.Game
-	sid				int
+	id				int
 }
 
 func (self *ShipAI) State() hal.Ship {
-	return self.game.GetShip(self.sid)
+	return self.game.GetShip(self.id)
 }
+
+// --------------------------------------------
+
+type PlanetAI struct {
+	mainAI			*AI
+	game			*hal.Game
+	id				int
+}
+
+func (self *PlanetAI) State() hal.Planet {
+	return self.game.GetPlanet(self.id)
+}
+
+// --------------------------------------------
 
 type AI struct {
 	game			*hal.Game
 	shipAIs			[]*ShipAI
+	planetAIs		[]*PlanetAI
 }
 
 func NewAI(game *hal.Game) *AI {
@@ -26,6 +44,7 @@ func NewAI(game *hal.Game) *AI {
 
 func (self *AI) Step() {
 	self.UpdateShipAIs()
+	self.UpdatePlanetAIs()
 }
 
 func (self *AI) UpdateShipAIs() {
@@ -38,8 +57,9 @@ func (self *AI) UpdateShipAIs() {
 
 	for _, sid := range my_new_ships {
 		ship_ai := new(ShipAI)
+		ship_ai.mainAI = self
 		ship_ai.game = game
-		ship_ai.sid = sid
+		ship_ai.id = sid
 		self.shipAIs = append(self.shipAIs, ship_ai)
 		game.Log("Turn %d: received ship %d", game.Turn(), sid)
 	}
@@ -50,7 +70,35 @@ func (self *AI) UpdateShipAIs() {
 		ship_ai := self.shipAIs[i]
 		if ship_ai.State().HP <= 0 {
 			self.shipAIs = append(self.shipAIs[:i], self.shipAIs[i+1:]...)
-			game.Log("Turn %d: ship %d destroyed", game.Turn(), ship_ai.sid)
+			i--
+			game.Log("Turn %d: ship %d destroyed", game.Turn(), ship_ai.id)
+		}
+	}
+}
+
+func (self *AI) UpdatePlanetAIs() {
+
+	game := self.game
+
+	if game.Turn() == 1 {
+
+		for _, planet := range game.AllPlanets() {
+			planet_ai := new(PlanetAI)
+			planet_ai.mainAI = self
+			planet_ai.game = game
+			planet_ai.id = planet.Id
+			self.planetAIs = append(self.planetAIs, planet_ai)
+		}
+	}
+
+	// Delete AIs with dead planets from the slice...
+
+	for i := 0; i < len(self.planetAIs); i++ {
+		planet_ai := self.planetAIs[i]
+		if planet_ai.State().HP <= 0 {
+			self.planetAIs = append(self.planetAIs[:i], self.planetAIs[i+1:]...)
+			i--
+			game.Log("Turn %d: planet %d destroyed", game.Turn(), planet_ai.id)
 		}
 	}
 }
--- run_game.bat
+++ run_game.bat
@@ -1,4 +1,4 @@
 
 go build mybot.go
-halite.exe -i "replays" -d "240 160" "MyBot.exe" "python python3bot/mybot.py"
+halite.exe -i "replays" -d "240 160" "MyBot.exe" ".\starterbot\MyBot.exe"
 
