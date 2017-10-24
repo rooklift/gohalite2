e47d04ed193ff8f2e75f226b4b0765e92a9884dc
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-10-24 15:51:51 +0100

    rearrange

--- ai/ai.go
+++ ai/ai.go
@@ -1,32 +1,17 @@
 package ai
 
 import (
-	"fmt"
-	"time"
-
 	hal "../gohalite2"
 )
 
-const (
-	NAME = "Fohristiwhirl"
-	VERSION = 2
-)
-
-func Run() {
-
-	game := hal.NewGame()
-
-	game.StartLog(fmt.Sprintf("log%d.txt", game.Pid()))
-	game.Log("--------------------------------------------------------------------------------")
-	game.Log("%s %d starting up at %s", NAME, VERSION, time.Now().Format("2006-01-02T15:04:05Z"))
-
-	fmt.Printf("%s %d\n", NAME, VERSION)
-
-	for {
-		game.Parse()
-		ai(game)
-		game.Send()
-	}
+type AI struct {
+	game	*hal.Game
 }
 
-func ai(game *hal.Game) {}
+func NewAI(game *hal.Game) *AI {
+	ret := new(AI)
+	ret.game = game
+	return ret
+}
+
+func (self *AI) Step() {}
---
+++ ai/startup.go
@@ -0,0 +1,32 @@
+package ai
+
+import (
+	"fmt"
+	"time"
+
+	hal "../gohalite2"
+)
+
+const (
+	NAME = "Fohristiwhirl"
+	VERSION = 2
+)
+
+func Run() {
+
+	game := hal.NewGame()
+
+	game.StartLog(fmt.Sprintf("log%d.txt", game.Pid()))
+	game.Log("--------------------------------------------------------------------------------")
+	game.Log("%s %d starting up at %s", NAME, VERSION, time.Now().Format("2006-01-02T15:04:05Z"))
+
+	fmt.Printf("%s %d\n", NAME, VERSION)
+
+	ai := NewAI(game)
+
+	for {
+		game.Parse()
+		ai.Step()
+		game.Send()
+	}
+}
