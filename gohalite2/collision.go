7613ebdd4c9300781ba35440b8b05124c17968a3
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-10-25 12:22:20 +0100

    AngleCollisionID()

---
+++ gohalite2/collision.go
@@ -0,0 +1,35 @@
+package gohalite2
+
+func (self *Game) AngleCollisionID(startx, starty float64, degrees int) int {
+
+	// Returns ID of the first planet we would collide with, or -1 if no hits
+
+	var collision_planets []Planet
+	all_planets := self.AllPlanets()
+
+	endx, endy := projection(startx, starty, 1000, degrees)
+
+	for _, planet := range all_planets {
+
+		collides := intersect_segment_circle(startx, starty, endx, endy, planet.X, planet.Y, planet.Radius)
+
+		if collides {
+			collision_planets = append(collision_planets, planet)
+		}
+	}
+
+	if len(collision_planets) == 0 {
+		return -1
+	}
+
+	closest_distance := 999999.9
+	closest_planet := Planet{}
+
+	for _, c := range collision_planets {
+		if dist(startx, starty, c.X, c.Y) < closest_distance {
+			closest_planet = c
+		}
+	}
+
+	return closest_planet.Id
+}
