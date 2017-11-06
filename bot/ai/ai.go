770e1be7bdab03d4051da62d94667fb0d8985eae
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-11-06 00:36:48 +0000

    v21 FINAL -- ecbfedb0cf1fac5b380166c54893f373a686b464

--- bot/ai/ai.go
+++ bot/ai/ai.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	NAME = "Fohristiwhirl"
-	VERSION = "21b3"			// (winner hash) "ecbfedb0cf1fac5b380166c54893f373a686b464" -- log 0
+	VERSION = "21 final"			// (winner hash) "ecbfedb0cf1fac5b380166c54893f373a686b464" -- log 0
 )
 
 func Run() {
