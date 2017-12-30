8854e79169efda4a9f3111587a9f4fccef1b6ce4
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-12-30 02:25:59 +0000

    replay

--- match4.py
+++ match4.py
@@ -3,8 +3,8 @@ import json, random, subprocess
 processes = [
 	"bot.exe",
 	"bot.exe",
-	".\\offbots\\brine\\brine.exe",
-	".\\offbots\\brine\\brine.exe",
+	".\\otherbots\\v48\\mybot.exe",
+	".\\otherbots\\v48\\mybot.exe",
 ]
 
 scores = [0,0,0,0]
