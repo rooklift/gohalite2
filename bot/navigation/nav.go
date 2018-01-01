8a015f6bd72ffbd8d18f8b9eb17049370ffa109c
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2018-01-01 16:31:23 +0000

    reduce safety margin in main nav

--- bot/navigation/nav.go
+++ bot/navigation/nav.go
@@ -20,7 +20,7 @@ func (self *NullNavStacker) AddToNavStack() {}
 
 func CheckEntityCollision(ship *hal.Ship, distance float64, degrees int, other hal.Entity) bool {		// Would we hit some specific entity?
 
-	const SAFETY_MARGIN = 0.001		// FIXME: not really needed now, can make this a lower value...
+	const SAFETY_MARGIN = 0.00001
 
 	endx, endy := hal.Projection(ship.X, ship.Y, distance, degrees)
 	return hal.IntersectSegmentCircle(ship.X, ship.Y, endx, endy, other.GetX(), other.GetY(), other.GetRadius() + hal.SHIP_RADIUS + SAFETY_MARGIN)
