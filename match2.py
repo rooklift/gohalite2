6176ed5b78f015d0bfa764411a7903611103c63b
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2018-01-04 12:44:23 +0000

    map_sizes

--- match2.py
+++ match2.py
@@ -1,15 +1,18 @@
 import json, random, subprocess
 
 processes = [
-	"bot.exe",
-	".\\otherbots\\v65\\mybot.exe --conservative",
+	"bot.exe --conservative",
+	".\\otherbots\\v67\\mybot.exe --conservative",
 ]
 
-WIDTH = 240			# 360
-HEIGHT = 160		# 240
+FORCED_WIDTH = None
+FORCED_HEIGHT = None
+
+# ------------------------------------------------------------------------
+
+map_sizes = [80, 80, 88, 88, 96, 96, 96, 104, 104, 104, 104, 112, 112, 112, 120, 120, 128, 128]
 
 scores = [0,0]
-
 positions = [0,1]
 
 print("{} --- {}".format(processes[0], processes[1]))
@@ -18,8 +21,16 @@ while 1:
 
 	random.shuffle(positions)
 
+	if (not FORCED_WIDTH) or (not FORCED_HEIGHT):
+		base_size = random.choice(map_sizes)
+		width = base_size * 3
+		height = base_size * 2
+	else:
+		width = FORCED_WIDTH
+		height = FORCED_HEIGHT
+
 	output = subprocess.check_output(
-		"halite.exe -d \"{} {}\" --no-compression -q \"{}\" \"{}\"".format(WIDTH, HEIGHT, processes[positions[0]], processes[positions[1]])
+		"halite.exe -d \"{} {}\" --no-compression -q \"{}\" \"{}\"".format(width, height, processes[positions[0]], processes[positions[1]])
 		).decode("ascii")
 
 	result = json.loads(output)
--- match4.py
+++ match4.py
@@ -7,8 +7,14 @@ processes = [
 	".\\otherbots\\v35\\mybot.exe",
 ]
 
-scores = [0,0,0,0]
+FORCED_WIDTH = None
+FORCED_HEIGHT = None
 
+# ------------------------------------------------------------------------
+
+map_sizes = [80, 80, 88, 88, 96, 96, 96, 104, 104, 104, 104, 112, 112, 112, 120, 120, 128, 128]
+
+scores = [0,0,0,0]
 positions = [0,1,2,3]
 
 print("{} --- {} --- {} --- {}".format(processes[0], processes[1], processes[2], processes[3]))
@@ -17,8 +23,16 @@ while 1:
 
 	random.shuffle(positions)
 
+	if (not FORCED_WIDTH) or (not FORCED_HEIGHT):
+		base_size = random.choice(map_sizes)
+		width = base_size * 3
+		height = base_size * 2
+	else:
+		width = FORCED_WIDTH
+		height = FORCED_HEIGHT
+
 	output = subprocess.check_output(
-		"halite.exe --no-compression -q \"{}\" \"{}\" \"{}\" \"{}\"".format(
+		"halite.exe -d \"{} {}\" --no-compression -q \"{}\" \"{}\" \"{}\" \"{}\"".format(width, height,
 			processes[positions[0]], processes[positions[1]], processes[positions[2]], processes[positions[3]]
 			)).decode("ascii")
 
